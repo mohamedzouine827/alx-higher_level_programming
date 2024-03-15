52a05d6d77f07e9b39a5271202e4249483d7c884
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-15 22:55:57 +0000

    8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 """Filter states by user input"""
 
-
 import MySQLdb
 from sys import argv
 
 if "__main__" == __name__:
-    # Establish connection to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
 
-    # Create a cursor object
     cur = db.cursor()
 
     s = argv[4]
 
-    # Execute the SQL query
-    query = ("SELECT * FROM states WHERE name LIKE = %s"
-             "ORDER BY id ASC"
-             .format(s))
-    cur.execute(query, (s,))
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    cur.execute(query, (s + '%',))
 
-    # Fetch all the rows
     rows = cur.fetchall()
 
-    # Print each row
     for row in rows:
         print(row)
 
-    # Close the cursor and connection
     cur.close()
     db.close()
