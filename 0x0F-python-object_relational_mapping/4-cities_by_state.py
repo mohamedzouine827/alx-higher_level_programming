94d6e627a2b205ec2ea80d1547718006548aa5d2
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-15 23:04:56 +0000

    8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Filter states by user input"""
+
+
+import MySQLdb
+from sys import argv
+
+if "__main__" == __name__:
+    data = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = data.cursor()
+
+    # Execute the query
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+
+    finaldata = cur.fetchall()
+
+    for dt in finaldata:
+        print(dt)
+
+    cur.close()
+    data.close()
