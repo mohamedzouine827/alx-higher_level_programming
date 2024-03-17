d59619ac60972366c7886a0570ac739df241c226
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-17 22:33:30 +0000

    7

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Model state """
+
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    a = sys.argv[1]
+    b = sys.argv[2]
+    c = sys.argv[3]
+    en = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        a,
+        b,
+        c),
+        pool_pre_ping=True)
+    Base.metadata.create_all(en)
+    nameFounded = sys.argv[4]
+
+    session = sessionmaker(bind=en)
+    s = session()
+
+    nameCoorect = s.query(State).filter(State.name == nameFounded).first()
+    if nameCoorect:
+        print("{}".format(nameFounded.id))
+    else:
+        print("Not found")
