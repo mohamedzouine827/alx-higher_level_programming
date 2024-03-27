39bb6d8fb11d85e57a0b2fa255f03c0c3de49987
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-27 23:46:45 +0000

    8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,5 +4,6 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
-    max = list_of_integers.sort()
-    return max[-1]
+    if list_of_integers:
+        max = list_of_integers.sort()
+        return max[-1]
