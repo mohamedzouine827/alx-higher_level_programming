810289638133c12f8679dc59801c5be20803da4d
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 01:20:19 +0000

    8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""urllib to fetch data"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {'email': argv[2]}
+    req = requests.post(argv[1], data=data)
+    print(req.text)
