95d6f92d1e1a7e58fd9109c34a33cd469cc773f8
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 01:23:04 +0000

    8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""urllib to fetch data"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    text = requests.get(url=argv[1])
+    code = text.status_code
+    if code >= 400:
+        print("Error code: ".format(code))
+    else:
+        print(text.text)
