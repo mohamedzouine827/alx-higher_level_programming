c7c703e1f2d6b7f2cfe37a0de3aec2778ab6fcd4
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 00:39:25 +0000

    8

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""urlib to get header"""
+
+
+import urllib.request as ul
+from sys import argv
+import urllib.error as error
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with ul.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error code: ", err)
