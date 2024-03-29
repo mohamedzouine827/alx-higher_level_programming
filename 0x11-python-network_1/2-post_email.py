6d0b6674a0ec59de1fac3170ab9a0ed657ad3f00
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 00:20:51 +0000

    8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""post request with urlib"""
+
+
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+if __name__ == "__main__":
+    email = argv[2]
+    url = argv[1]
+
+    data = {"email": email}
+
+    values = urlencode(data)
+    values = values.encode('ascii')
+
+    req = Request(url, data)
+    with urlopen(req) as response:
+        html = response.read()
+
+    print(html.decode('utf-8'))
