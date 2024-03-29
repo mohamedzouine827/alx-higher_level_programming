8567d30e59a48d3b66db0b5f742e0da75c42f00d
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 01:15:51 +0000

    7

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""urllib to fetch data"""
+
+import requests
+from sys import argv
+
+url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    text = requests.get(url=argv[1])
+    texts = text.headers.get("X-Request-Id")
+    print(texts)
