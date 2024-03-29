3ea888ffc856e27cbee7796627f05e9587fcafea
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 01:10:28 +0000

    8

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""urllib to fetch data"""
+
+import requests
+
+url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    text = requests.get(url=url)
+    texts = text.text
+    print("Body response:")
+    print("\t- type: {}".format(type(texts)))
+    print("\t- content: {}".format(texts))
