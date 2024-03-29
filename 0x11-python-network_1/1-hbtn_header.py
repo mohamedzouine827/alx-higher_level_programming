b8cfdbab0c0a6f97efc40ab72aae3406f3b95f61
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-29 00:04:23 +0000

    8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""urlib to get header"""
+
+
+import urllib.request as ul
+from sys import argv
+
+url = argv[1]
+
+with ul.urlopen(url) as response:
+    header = response.getheader('X-Request-Id')
+    print(header)
